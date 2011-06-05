9cdc5115e27287e3f1a4e9c259f1951a010f9c3a
Jerome Flesch <jflesch@gmail.com>
2011-06-05 13:18:41 +0200

    skeleton

--- src/dtgrep.py
+++ src/dtgrep.py
@@ -3,18 +3,46 @@
 import sys
 
 import pygtk
+import glib
 import gtk
 
 pygtk.require("2.0")
 
-if __name__ == "__main__":
-    uifile = "dtgrep.glade"
-    wTree = gtk.Builder()
-    wTree.add_from_file(uifile)
+POSSIBLE_UI_FILES = \
+[
+    "./dtgrep.glade",
+    "src/dtgrep.glade",
+    "/usr/local/share/dtgrep/dtgrep.glade",
+    "/usr/share/dtgrep/dtgrep.glade",
+]
 
+def load_uifile():
+    wTree = gtk.Builder()
+    has_uifile = False
+    for uifile in POSSIBLE_UI_FILES:
+        try:
+            wTree.add_from_file(uifile)
+        except glib.GError, e:
+            print "Try to used UI file %s but failed: %s" % (uifile, str(e))
+            continue
+        has_uifile = True
+        print "UI file used: "+uifile
+        break
+    if not has_uifile:
+        raise Exception("Can't find ressource file. Aborting")
+    return wTree
+
+def connect_signals(wTree):
     window = wTree.get_object("MainWindow")
-    if window:
-        window.connect("destroy", gtk.main_quit)
-        window.set_visible(True)
-    
+    assert(window)
+    window.connect("destroy", gtk.main_quit)
+    window.set_visible(True)
+
+def main():
+    wTree = load_uifile()
+    connect_signals(wTree)
     gtk.main()
+
+
+if __name__ == "__main__":
+    main()

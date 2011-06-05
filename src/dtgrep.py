2639777d28de231b8bfb79948608214793e3561d
Jerome Flesch <jflesch@gmail.com>
2011-06-05 14:47:09 +0200

    add about dialog

--- src/dtgrep.py
+++ src/dtgrep.py
@@ -33,16 +33,50 @@ def load_uifile():
         raise Exception("Can't find ressource file. Aborting")
     return wTree
 
-def connect_signals(wTree):
-    wTree.get_object("mainWindow").connect("destroy", gtk.main_quit)
-    wTree.get_object("toolbuttonQuit").connect("clicked", gtk.main_quit)
-    wTree.get_object("menuitemQuit").connect("activate", gtk.main_quit)
+def destroy_wTree(wTree):
+    wTree.get_object("mainWindow").destroy()
+    wTree.get_object("aboutdialog").destroy()
+    wTree.get_object("windowSearch").destroy()
+    wTree.get_object("windowSettings").destroy()
+
+class AboutDialog:
+    def __init__(self):
+        # we have to recreate new dialogs each time, otherwise, when user
+        # destroy the dialog, we won't be able to redisplay it
+        # TODO(Jflesch): memleak
+        self.wTree = load_uifile()
+        self.aboutDialog = self.wTree.get_object("aboutdialog")
+        assert(self.aboutDialog)
+        self.connect_signals()
+        self.aboutDialog.set_visible(True)
+
+    def connect_signals(self):
+        self.aboutDialog.connect("response", lambda x, y: self.destroy())
+        self.aboutDialog.connect("close", lambda x: self.destroy())
+
+    def destroy(self):
+        destroy_wTree(self.wTree)
+
+class MainWindow:
+    def __init__(self):
+        self.wTree = load_uifile()
+        self.mainWindow = self.wTree.get_object("mainWindow")
+        assert(self.mainWindow)
+        self.connect_signals()
+        self.mainWindow.set_visible(True)
+
+    def connect_signals(self):
+        self.mainWindow.connect("destroy", lambda x: self.destroy())
+        self.wTree.get_object("toolbuttonQuit").connect("clicked", lambda x: self.destroy())
+        self.wTree.get_object("menuitemQuit").connect("activate", lambda x: self.destroy())
+        self.wTree.get_object("menuitemAbout").connect("activate", lambda x: AboutDialog())
+
+    def destroy(self):
+        destroy_wTree(self.wTree)
+        gtk.main_quit()
+
 
 def main():
-    wTree = load_uifile()
-    connect_signals(wTree)
-
-    wTree.get_object("mainWindow").set_visible(True)
-
+    MainWindow()
     gtk.main()
 

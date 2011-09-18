4cd3674fd2ab12796eaf1e73e3106e245511aed8
Jerome Flesch <jflesch@gmail.com>
2011-09-18 11:06:02 +0200

    main window : make the page zoomable

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -21,8 +21,11 @@ class MainWindow:
         self.pageListUI = self.wTree.get_object("treeviewPage")
         self.pageScrollWin = self.wTree.get_object("scrolledwindowPageImg")
         self.pageImg = self.wTree.get_object("imagePageImg")
+        self.pageEventBox = self.wTree.get_object("eventboxImg")
         self.pageTxt = self.wTree.get_object("textviewPageTxt")
         self.pageVpaned = self.wTree.get_object("vpanedPage")
+
+        self.page_scaled = True
 
         self._connect_signals()
         self.mainWindow.set_visible(True)
@@ -64,20 +67,26 @@ class MainWindow:
 
     def _show_page_img(self, page):
         filepath = self.doc.get_img_path(page)
-        pixbuf = gtk.gdk.pixbuf_new_from_file(filepath)
 
-        # we strip 30 pixels from the width of scrolled window, because the vertical scrollbar
-        # is not included
-        # TODO(Jflesch): Figure out a way to get the exact scrollbar width
-        wantedWidth = self.pageScrollWin.get_allocation().width - 30;
-        if pixbuf.get_width() > wantedWidth:
-            ratio = float(wantedWidth) / pixbuf.get_width();
-            wantedHeight = int(ratio * pixbuf.get_height())
+        if self.page_scaled:
+            pixbuf = gtk.gdk.pixbuf_new_from_file(filepath)
+            # we strip 30 pixels from the width of scrolled window, because the vertical scrollbar
+            # is not included
+            # TODO(Jflesch): Figure out a way to get the exact scrollbar width
+            wantedWidth = self.pageScrollWin.get_allocation().width - 30;
+            if pixbuf.get_width() > wantedWidth:
+                ratio = float(wantedWidth) / pixbuf.get_width();
+                wantedHeight = int(ratio * pixbuf.get_height())
+                self.pageImg.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+            else:
+                wantedWidth = pixbuf.get_width()
+                wantedHeight = pixbuf.get_height()
+                self.pageImg.window.set_cursor(None)
+            scaled_pixbuf = pixbuf.scale_simple(wantedWidth, wantedHeight, gtk.gdk.INTERP_BILINEAR)
+            self.pageImg.set_from_pixbuf(scaled_pixbuf)
         else:
-            wantedWidth = pixbuf.get_width()
-            wantedHeight = pixbuf.get_height()
-        scaled_pixbuf = pixbuf.scale_simple(wantedWidth, wantedHeight, gtk.gdk.INTERP_BILINEAR)
-        self.pageImg.set_from_pixbuf(scaled_pixbuf)
+            self.pageImg.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+            self.pageImg.set_from_file(filepath)
         self.pageImg.show()
 
     def _show_page_txt(self, page):
@@ -100,6 +109,8 @@ class MainWindow:
         if page == 0:
             page = self._get_current_page()
 
+        self.page_scaled = True
+
         print "Showing page %d" % (page)
 
         self.pageListUI.get_selection().select_path((page-1))
@@ -115,6 +126,11 @@ class MainWindow:
         self._show_page_img(self._get_current_page())
         self._reset_vpaned()
 
+    def _change_scale(self, objsrc = None, x = None, y = None):
+        print "Changing scaling: %d -> %d" % (self.page_scaled, not self.page_scaled)
+        self.page_scaled = not self.page_scaled
+        self._show_page_img(self._get_current_page())
+
     def _connect_signals(self):
         self.mainWindow.connect("destroy", lambda x: self._destroy())
         self.wTree.get_object("toolbuttonQuit").connect("clicked", lambda x: self._destroy())
@@ -127,6 +143,7 @@ class MainWindow:
         self.wTree.get_object("toolbuttonSearch").connect("clicked", self._open_search_window)
         self.wTree.get_object("menuitemSearch").connect("activate", self._open_search_window)
         self.pageListUI.connect("cursor-changed", self._show_page)
+        self.pageEventBox.connect("button-press-event", self._change_scale)
 
     def _destroy(self):
         self.wTree.get_object("mainWindow").destroy()

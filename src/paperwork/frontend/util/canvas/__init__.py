106764f947c678ea5c02d8778ae260c239a097db
Jerome Flesch <jflesch@gmail.com>
2014-06-09 09:10:53 +0200

    Canvas: Reduce the framerate of animations
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/paperwork/frontend/util/canvas/__init__.py
+++ src/paperwork/frontend/util/canvas/__init__.py
@@ -72,7 +72,7 @@ class Canvas(Gtk.DrawingArea, Gtk.Scrollable):
         'window-moved': (GObject.SignalFlags.RUN_LAST, None, ()),
     }
 
-    TICK_INTERVAL = (1000 / 15)
+    TICK_INTERVAL = (1000 / 10)
 
     def __init__(self, scrollbars):
         Gtk.DrawingArea.__init__(self)

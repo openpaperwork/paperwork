7ab9af32faef647e67d2bc1ba0d72734d5d30158
jflesch <jflesch@gmail.com>
2015-11-13 13:45:45 +0100

    Label editor: Rewrite the .glade file with Glade + Fix warnings raised by Glade
    Signed-off-by: jflesch <jflesch@gmail.com>

--- src/paperwork/frontend/labeleditor/__init__.py
+++ src/paperwork/frontend/labeleditor/__init__.py
@@ -51,8 +51,7 @@ class LabelEditor(object):
 
         dialog = widget_tree.get_object("dialogLabelEditor")
         dialog.set_transient_for(main_window)
-        self.__cancel_button = widget_tree.get_object("properties-label-button-cancel")
-        self.__ok_button = widget_tree.get_object("properties-label-button-ok")
+        self.__ok_button = widget_tree.get_object("buttonOk")
 
         color_chooser = widget_tree.get_object("labelColorChooser")
         color_chooser.set_rgba(self.label.color)

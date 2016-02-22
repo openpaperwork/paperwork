8eb2f1531438bdabc6f02e6dbe735a0453d19581
Jerome Flesch <jflesch@gmail.com>
2016-02-22 23:37:25 +0100

    Backend: DocSearch: Make it possible to specify the indexdir when instantiating the DocSearch object
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>
    
    # Conflicts:
    #	src/paperwork/frontend/mainwindow/__init__.py

--- src/paperwork/backend/docsearch.py
+++ src/paperwork/backend/docsearch.py
@@ -345,7 +345,8 @@ class DocSearch(object):
         last_read=whoosh.fields.DATETIME(stored=True),
     )
 
-    def __init__(self, rootdir, callback=dummy_progress_cb):
+    def __init__(self, rootdir, indexdir=None,
+                 callback=dummy_progress_cb):
         """
         Index files in rootdir (see constructor)
 
@@ -359,9 +360,13 @@ class DocSearch(object):
                     being read
         """
         self.rootdir = rootdir
-        base_data_dir = os.getenv("XDG_DATA_HOME",
-                                  os.path.expanduser("~/.local/share"))
-        self.indexdir = os.path.join(base_data_dir, "paperwork", "index")
+        self.indexdir = indexdir
+        if self.indexdir is None:
+            base_data_dir = os.getenv(
+                "XDG_DATA_HOME",
+                os.path.expanduser("~/.local/share")
+            )
+            self.indexdir = os.path.join(base_data_dir, "paperwork", "index")
         mkdir_p(self.indexdir)
 
         self._docs_by_id = {}  # docid --> doc

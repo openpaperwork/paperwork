6b32c55647dc114e36c28b9932ac87a3be152051
Jerome Flesch <jflesch@gmail.com>
2016-02-25 22:53:23 +0100

    0.3.1 --> 0.3.1-git
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # * change it also in
     #   src/paperwork/frontend/aboutdialog/aboutdialog.glade
     # * update the archive list in the README
-    version="0.3.1",
+    version="0.3.1-git",
     description=(
         "Using scanner and OCR to grep dead trees the easy way (Linux only)"
     ),

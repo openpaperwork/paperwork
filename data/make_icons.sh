cfff777b4862e381b87348b23f2bf0f10e85d554
Jerome Flesch <jflesch@gmail.com>
2016-09-22 00:10:29 +0200

    Add script to generate icons in various sizes
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

---
+++ data/make_icons.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for size in 16 24 30 32 48 50 150 256 ; do
+	echo "Generating icon ${size}x${size} ..."
+	inkscape -w ${size} -h ${size} -e paperwork_${size}.png paperwork.svg
+	if [ ${size} -lt 256 ]; then  # max size for .ico files
+		convert paperwork_${size}.png paperwork_${size}.ico
+		rm -f paperwork_${size}.png
+	fi
+done

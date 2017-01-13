e64a734c4f5a481087d62e01329bafe2885aa302
Jerome Flesch <jflesch@openpaper.work>
2017-01-13 13:22:13 +0100

    This branch will become Paperwork-gui 1.2
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- src/paperwork/frontend/mainwindow/__init__.py
+++ src/paperwork/frontend/mainwindow/__init__.py
@@ -79,7 +79,7 @@ _ = gettext.gettext
 logger = logging.getLogger(__name__)
 
 
-__version__ = '1.1-git'
+__version__ = '1.2-git'
 
 
 # during tests, we have multiple instatiations of MainWindow(), but we must
